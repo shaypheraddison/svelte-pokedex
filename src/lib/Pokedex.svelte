0a789f0896a35d838937e2fd7123e0033a498dc8
shaypheraddison <shaypherashe@gmail.com>
2024-09-08 00:14:20 -0500

    updated layout of component Pokedex

--- src/lib/Pokedex.svelte
+++ src/lib/Pokedex.svelte
@@ -1,81 +1,79 @@
 <script>
-    export let pokemon = "";
-    // import Button from "./Api.svelte";
+  export let pokemon = "";
 
-    let pokemonData;
-    let pokemonTypes = [];
-    let pokemonTypeRelations = {};
+  let pokemonData;
+  let pokemonTypes = [];
+  let pokemonTypeRelations = {};
 
-    async function fetchPokemonData() {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`);
-        pokemonData = await response.json();
+  async function fetchPokemonData() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`);
+      pokemonData = await response.json();
 
-        pokemonTypes = [];
-        for (const typeName of pokemonData.types) {
-          pokemonTypes.push(typeName.type.name);
-          pokemonTypes = pokemonTypes;
-        };
-
-        await damageRelations();
-
-        pokemon = "";
-    }
-
-    async function damageRelations() {
-      pokemonTypeRelations = {}; 
-      
-      for (const type of pokemonTypes) {
-        const damageResponse = await fetch(`https://pokeapi.co/api/v2/type/${type.toLowerCase()}/`);
-        const typeData = await damageResponse.json();
-
-        pokemonTypeRelations[type] = typeData.damage_relations
+      pokemonTypes = [];
+      for (const typeName of pokemonData.types) {
+        pokemonTypes.push(typeName.type.name);
+        pokemonTypes = pokemonTypes;
       };
-    }
+
+      await damageRelations();
+
+      pokemon = "";
+  }
+
+  async function damageRelations() {
+    
+    for (const type of pokemonTypes) {
+      const damageResponse = await fetch(`https://pokeapi.co/api/v2/type/${type.toLowerCase()}/`);
+      let typeData = await damageResponse.json();
+
+      pokemonTypeRelations[type] = typeData.damage_relations
+    };
+  }
     
     
 </script>
 
 <main>
-    <div class="kalos-dex">
-      <img class="top-dex" src="../src/assets/kalosdex-top.png" alt="top-dex">
-        <div class="screen">
-          <div class="pokemon-search">
-            <input class="pokemon-name" type="text" bind:value={pokemon} placeholder="Enter Pokemon Name">
-            <button on:click={fetchPokemonData}></button>
-          </div>
-            <div class="screen-content">
-                {#if pokemonData}
-                <div>
-                    <img class="sprite" src="{pokemonData.sprites.front_default}" alt="{pokemonData.name}">
-                </div>
-                <div class="pokemon-info">
-                    <h1 class="capitalize">{pokemonData.name}</h1>
-                    <ul>
-                        <li class="capitalize">Pokedex Number: {pokemonData.id}</li>
-                        <li class="capitalize">Type: 
-                          {#each pokemonTypes as type}
-                            {type + " "}
-                          {/each}
-                        </li>
-                        {#each Object.keys(pokemonTypeRelations) as damageType}
-                          <li class="capitalize">Super Effective Against:
-                            {#each pokemonTypeRelations[damageType].double_damage_to as superE}
-                              {superE.name + " "}
-                            {/each}
-                          </li>
-                          <li class="capitalize">Weak Against: 
-                            {#each pokemonTypeRelations[damageType].double_damage_from as weakA}
-                              {weakA.name + " "}
-                            {/each}
-                          </li>
-                        {/each}
-                    </ul>
-                </div>
-                {/if}
-            </div>
+  <div class="kalos-dex">
+    <img class="top-dex" src="../src/assets/kalosdex-top.png" alt="top-dex">
+      <div class="screen">
+        <div class="pokemon-search">
+          <input class="pokemon-name" type="text" bind:value={pokemon} placeholder="Enter Pokemon Name">
+          <button on:click={fetchPokemonData}></button>
         </div>
-      <img class="bottom-dex" src="../src/assets/kalosdex-bottom.png" alt="bottom-dex">
-    </div> 
+          <div class="screen-content">
+          {#if pokemonData}
+            <div>
+              <img class="sprite" src="{pokemonData.sprites.front_default}" alt="{pokemonData.name}">
+            </div>
+            <div class="pokemon-info">
+              <h1 class="capitalize">{pokemonData.name}</h1>
+              <ul>
+                <li class="capitalize">Pokedex Number: {pokemonData.id}</li>
+                <li class="capitalize">Type: 
+                  {#each pokemonTypes as type}
+                    {type + " "}
+                  {/each}
+                </li>
+                {#each Object.keys(pokemonTypeRelations) as damageType}
+                <li class="capitalize">Super Effective Against:
+                  {#each pokemonTypeRelations[damageType].double_damage_to as superE}
+                    {superE.name + " "}
+                  {/each}
+                </li>
+                <li class="capitalize">Weak Against: 
+                  {#each pokemonTypeRelations[damageType].double_damage_from as weakA}
+                    {weakA.name + " "}
+                  {/each}
+                </li>
+                {/each}
+              </ul>
+            </div>
+          {/if}
+        </div>
+      </div>
+    <img class="bottom-dex" src="../src/assets/kalosdex-bottom.png" alt="bottom-dex">
+  </div> 
 </main>
 
 
@@ -84,8 +82,8 @@
     position: relative;
     visibility:visible;
   }
-
   main {
+    /* margin-top: -100px; */
     color: black;
   }
   li {
@@ -98,7 +96,7 @@
     align-items: center;
     background-color: aquamarine;
     width: 440px;
-    height: 800px;
+    height: 900px;
     border-radius: 50px;
   }
 
